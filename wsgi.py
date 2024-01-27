5f4c00c1bcff744e05c819a7e09e6892194c6672
mmrzarzn <157893869+mmrzarzn@users.noreply.github.com>
2024-01-27 18:27:50 +0330

    Add files via upload
    new

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Rase project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rase.settings')
+
+application = get_wsgi_application()
